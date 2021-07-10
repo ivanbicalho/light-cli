ed714d380767ccd8ccb1ba1aa021ed910c356f55
Ivan Ribeiro Bicalho <ivanribeirob@gmail.com>
2021-07-10 11:58:19 -0300

    Fix publish file

--- src/LightCli/publish.ps1
+++ src/LightCli/publish.ps1
@@ -4,4 +4,5 @@ rm publish -r
 dotnet build .
 dotnet pack -c Release -o publish
 cd publish
-nuget push *.nupkg -Source https://api.nuget.org/v3/index.json -ApiKey [System.Environment]::GetEnvironmentVariable('NUGET_API_KEY').ToString()
+$key=[System.Environment]::GetEnvironmentVariable('NUGET_API_KEY').ToString()
+nuget push *.nupkg -Source https://api.nuget.org/v3/index.json -ApiKey $key
